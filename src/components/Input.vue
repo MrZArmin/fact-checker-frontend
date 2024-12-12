2b8137f337b600714f07516ce1774d7d6123b298
smokk <pdc.levike@gmail.com>
2024-12-12 23:27:48 +0100

    fixes v2

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="input">
     <div class="input-container">
-      <textarea v-model="text" class="input-text" placeholder="Mit szeretnél megtudni?" @keydown.enter="handleSend"></textarea>
+      <textarea v-model="text" class="input-text" :placeholder=placeholderText
+        @keydown="handleKeydown"
+        @input="autoResize"
+        ref="textarea">
+      </textarea>
       <div class="icon-container">
         <i @click="handleSend" class="icon send white large" :class="{ disabled: !text.trim() }"/>
       </div>
@@ -14,11 +18,36 @@ import { ref } from 'vue';
 
 const emit = defineEmits(['send']);
 const text = ref('');
+const textarea = ref(null);
+const placeholderText = "Mit szeretnél megtudni?";
 
 const handleSend = () => {
   if (text.value.trim().length > 0) {
     emit('send', text.value);
     text.value = '';
+    resizeTextAreaToDefault();
   }
 };
+
+const handleKeydown = (e) => {
+  if (e.key === 'Enter' && !e.shiftKey) {
+    e.preventDefault();
+    handleSend();
+  }
+};
+
+const autoResize = () => {
+  const el = textarea.value;
+  el.style.height = 'auto';
+  el.style.height = el.scrollHeight + 'px';
+  el.style.overflow = "auto";
+};
+
+const resizeTextAreaToDefault = () => {
+  const el = textarea.value;
+  el.style.height = "30px";
+  el.style.overflow = "hidden";
+}
+
+//TODO classokkal megcsinálni, vagy jó így?
 </script>
